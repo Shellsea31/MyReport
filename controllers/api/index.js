a989ad2416ba529d571cf6642cf92c62e42c240d
Shelsy Elaine <elainesbarrera@gmail.com>
2021-07-07 16:48:29 -0700

    merged

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js'); 
+const reportRoutes = require('./report-routes'); 
 
 // prefixes routes
 router.use('/users', userRoutes); 
+router.use('/reports', reportRoutes)
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User'); 
+const Report = require('./Report'); 
 
 module.exports = User; 
