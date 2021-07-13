84789b580d30c1f3afe31d2bcdc190fe8a873195
Shelsy Elaine <elainesbarrera@gmail.com>
2021-07-12 18:39:26 -0700

    ADDED: basic pages setup

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <h1>You Are in Home</h1>
+    )
+}
+
+export default Home
---
+++ client/src/Pages/ReportDash.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportDash = () => {
+    return (
+        <h1>You Are in Report Dashboard</h1>
+    )
+}
+
+export default ReportDash
